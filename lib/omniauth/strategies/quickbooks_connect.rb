e968b5fd3b85720185dfd95143d6042df540528c
seb-sykio <s.delcour@sykio.fr>
2018-03-15 10:33:06 +0100

    test strategy

--- lib/omniauth/strategies/quickbooks_connect.rb
+++ lib/omniauth/strategies/quickbooks_connect.rb
@@ -5,71 +5,41 @@ module OmniAuth
     class QuickbooksConnect < OmniAuth::Strategies::OAuth2
       option :name, :quickbooks_connect
 
-      option :client_options, {
-        :access_token_path  => "/oauth/v1/get_access_token",
-        :authorize_url      => "https://appcenter.intuit.com/Connect/Begin",
-        :request_token_path => "/oauth/v1/get_request_token",
-:site => "https://oauth.intuit.com",
-      }
+      option(
+        :client_options,
+        {
+          site: 'https://appcenter.intuit.com/connect/oauth2',
+          authorize_url: 'https://appcenter.intuit.com/connect/oauth2',
+          token_url: 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
+        },
+      )
 
-      option :provider_ignores_state, true
-
-
-     
-      uid { access_token.params['realmId'] }
-
-
+      uid { request.params['realmId'] }
 
       info do
-        {
-          :email => raw_info[:email],
-          :organisation_name => raw_info[:organisation_name],
-          :given_name => raw_info['given_name'],
-          :family_name => raw_info['family_name'],
-        }
-      end
-
-      def raw_info
-        @raw_info ||= deep_symbolize(access_token.params)
+        raw_info
       end
 
       extra do
-        e = {
-          :raw_info => raw_info
-        }
-        #e[:extra_info] = extra_info unless skip_info?
-
-        e
+        { raw_info: raw_info }
       end
 
-
-      def redirect_params
-        if options.key?(:callback_path) || OmniAuth.config.full_host
-          {:redirect_uri => callback_url}
-        else
+      def raw_info
+        @raw_info ||= options.scope.split(/\s+/).include?('openid') ?
+          JSON.parse(access_token.get("https://#{accounts_domain}/v1/openid_connect/userinfo").body) :
           {}
-        end
       end
 
-      # NOTE: We call redirect_params AFTER super in these methods intentionally
-      # the OAuth2 strategy uses the authorize_params and token_params methods
-      # to set up some state for testing that we need in redirect_params
-
-      def authorize_params
-        params = super
-        params = params.merge(request.params) unless OmniAuth.config.test_mode
-        redirect_params.merge(params)
-      end
-
-      def request_phase
-        redirect client.auth_code.authorize_url(authorize_params)
-      end
-
-      # Required for omniauth-oauth2 >= 1.4
-      # https://github.com/intridea/omniauth-oauth2/issues/81
       def callback_url
-        full_host + script_name + callback_path
+        options[:redirect_uri] || (full_host + script_name + callback_path)
       end
+
+      private
+
+      def accounts_domain
+        false == options.sandbox ? 'accounts.platform.intuit.com' : 'sandbox-accounts.platform.intuit.com'
+      end
+
     end
   end
 end
